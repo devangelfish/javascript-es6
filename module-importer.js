30636aeb1af71dac91a7ae82c7b8d324ad784cd7
devangelfish <jaebeommun@gmail.com>
2021-06-09 22:31:53 +0900

    init: ES6 커먼JS 모듈

---
+++ module-importer.js
@@ -0,0 +1,6 @@
+const printName = require('./module')
+
+var first = '빅토르'
+var last = '위고'
+
+printName({first, last})
---
+++ module.js
@@ -0,0 +1,3 @@
+const printName = name => console.log(`${name.first}${name.last}님 안녕하세요`)
+
+module.exports = printName
